2b3209182d3fdc31c907bc67a738287501c35088
lightScout <juanmullercs@hotmail.com>
2021-04-05 13:01:03 +0100

    .implement circular reveal

--- app/src/main/java/com/lightscout/radioreaction101/GreenFragment.kt
+++ app/src/main/java/com/lightscout/radioreaction101/GreenFragment.kt
@@ -1,8 +1,10 @@
 package com.lightscout.radioreaction101
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewAnimationUtils
 import androidx.dynamicanimation.animation.FloatPropertyCompat
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
@@ -10,47 +12,60 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navOptions
+import kotlin.math.hypot
 
 class GreenFragment : Fragment(R.layout.green_fragment_layout) {
 
     val args: GreenFragmentArgs by navArgs()
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("TAG_J", "GreenF: pivot points of touch - pX = ${args.pivotPoints[0]} pY = ${args.pivotPoints[1]} ")
-        view.pivotX = args.pivotPoints[0]
-        view.pivotY = args.pivotPoints[1]
-        getSpringAnimation(view, SpringAnimation.SCALE_Y, 1F).start()
-        getSpringAnimation(view, SpringAnimation.SCALE_X, 1F).start()
+//        Log.d("TAG_J", "GreenF: pivot points of touch - pX = ${args.pivotPoints[0]} pY = ${args.pivotPoints[1]} ")
+//        view.pivotX = args.pivotPoints[0]
+//        view.pivotY = args.pivotPoints[1]
+//        getSpringAnimation(view, SpringAnimation.SCALE_Y, 1F).start()
+//        getSpringAnimation(view, SpringAnimation.SCALE_X, 1F).start()
 
-        view.setOnClickListener {
+
+        val cx = args.pivotPoints?.get(0)
+        val cy = args.pivotPoints?.get(1)
+        if (cx != null && cy != null) {
+            createCircularRevealAnim(view, cx, cy, 0f)
+        }
+        view.setOnTouchListener { view, motionEvent ->
             val options = navOptions {
                 anim {
-                    enter = R.anim.appear
                     exit = R.anim.gone
-                    popEnter = R.animator.nav_default_pop_enter_anim
-                    popExit = R.animator.nav_default_pop_exit_anim
                 }
             }
-            view.findNavController().navigate(R.id.redFragment, null, options)
-        }
+            view.findNavController().navigate(
+                GreenFragmentDirections.actionGreenFragmentToRedFragment2()
+                    .setPivotPoints(floatArrayOf(motionEvent.x, motionEvent.y)), options
+            )
 
+            false
+        }
 
     }
 
-    private fun getSpringAnimation(
-        view: View,
-        springAnimationType: FloatPropertyCompat<View>,
-        finalPosition: Float
-    ): SpringAnimation {
-        return SpringAnimation(view, springAnimationType).setStartValue(0F).also {
-            val spring = SpringForce()
-            spring.finalPosition = finalPosition
-            spring.stiffness = SpringForce.STIFFNESS_VERY_LOW // optional
-            spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY // optional
-            it.spring = spring
-        }
+    fun createCircularRevealAnim(view: View, cx: Float, cy: Float, startRadius: Float) {
 
+        // get the final radius of the clipping circle
+        val finalRadius = hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+        // create the animator for this view (the start radius is zero)
+        val anim = ViewAnimationUtils.createCircularReveal(
+            view,
+            cx.toInt(),
+            cy.toInt(),
+            startRadius,
+            finalRadius
+        )
+//        view.visibility = View.VISIBLE
+//        Log.d("TAG_J", "on click: ${ view.visibility } ")
+
+        anim.start()
 
     }
 }
--- app/src/main/java/com/lightscout/radioreaction101/RedFragment.kt
+++ app/src/main/java/com/lightscout/radioreaction101/RedFragment.kt
@@ -1,69 +1,81 @@
 package com.lightscout.radioreaction101
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.transition.Explode
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.dynamicanimation.animation.FloatPropertyCompat
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.navOptions
+import kotlin.math.hypot
 
 class RedFragment : Fragment(R.layout.red_fragment_layout) {
 
+    val args: RedFragmentArgs by navArgs()
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        val cx = args.pivotPoints?.get(0)
+        val cy = args.pivotPoints?.get(1)
+        if (cx != null && cy != null) {
+            createCircularRevealAnim(view, cx, cy, 0f)
+        }
+
+
+
+
         view.setOnTouchListener { view, motionEvent ->
-
-
-            Log.d("TAG_J", "onViewCreated: ")
 
             val options = navOptions {
                 anim {
-                    enter = R.anim.appear
                     exit = R.anim.gone
-                    popEnter = R.animator.nav_default_pop_enter_anim
-                    popExit = R.animator.nav_default_pop_exit_anim
                 }
             }
-            Log.d("TAG_J", "RedF: pivot points of touch - pX = ${motionEvent.x} pY = ${motionEvent.y} ")
-            val action = RedFragmentDirections.actionRedFragmentToGreenFragment(floatArrayOf(motionEvent.x, motionEvent.y))
-            view.findNavController().navigate(action)
+//
+            val action = RedFragmentDirections.actionRedFragmentToGreenFragment().setPivotPoints(
+                floatArrayOf(
+                    motionEvent.x,
+                    motionEvent.y
+                )
+            )
+            if (view.findNavController().currentDestination?.id == R.id.redFragment) {
+                view.findNavController().navigate(action, options)
+            }
+
+//            Log.d("TAG_J", "nav_controller id : ${} / ${}")
             false
         }
     }
 
 
-//            getSpringAnimation(it, SpringAnimation.SCALE_Y, 0F).start()
-//            getSpringAnimation(it, SpringAnimation.SCALE_X, 0F).start().also {
-//
-//
-//
-//            }
-
-    }
+}
 
 
+fun createCircularRevealAnim(view: View, cx: Float, cy: Float, startRadius: Float) {
 
-    private fun getSpringAnimation(
-        view: View,
-        springAnimationType: FloatPropertyCompat<View>,
-        finalPosition: Float
-    ): SpringAnimation {
-        return SpringAnimation(view, springAnimationType).setStartValue(1F).also {
-            val spring = SpringForce()
-            spring.finalPosition = finalPosition
-            spring.stiffness = SpringForce.STIFFNESS_VERY_LOW // optional
-            spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY // optional
-            it.spring = spring
-        }
+    // get the final radius of the clipping circle
+    val finalRadius = hypot(cx.toDouble(), cy.toDouble()).toFloat()
 
+    // create the animator for this view (the start radius is zero)
+    val anim = ViewAnimationUtils.createCircularReveal(
+        view,
+        cx.toInt(),
+        cy.toInt(),
+        startRadius,
+        finalRadius
+    )
+//        view.visibility = View.VISIBLE
+//        Log.d("TAG_J", "on click: ${ view.visibility } ")
 
-    }
+    anim.start()
+
+}
